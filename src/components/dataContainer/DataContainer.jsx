a1c4df89cc0c67053a093776711c03d3b349e160
vasco.mascarenhas <vasco.mascarenhas@hotmail.com>
2023-10-14 16:35:14 +0100

    div to aside

--- src/components/dataContainer/DataContainer.jsx
+++ src/components/dataContainer/DataContainer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./datacontainer.css";
 const DataContainer = ({ children }) => {
-  return <div className="data-container">{children}</div>;
+  return <aside className="data-container">{children}</aside>;
 };
 
 export default DataContainer;
